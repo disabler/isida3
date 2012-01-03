8037b07dda313eef3a51698601b84cdb4e25a61b
disabler <dissy@ya.ru>
2012-01-04 01:34:05 +0400

    add: execute action in `acl` after update

--- plugins/acl.py
+++ plugins/acl.py
@@ -87,6 +87,15 @@ def acl_add_del(jid,text,flag):
 		if atime: msg += ' [%s] %s %s %s -> %s' % (time.ctime(atime),acl_cmd, acl_sub_act, text[0], ' '.join(text[1:]).replace('%20','\ ').replace('\n',' // '))
 		else: msg += ' %s %s %s -> %s' % (acl_cmd, acl_sub_act, text[0], ' '.join(text[1:]).replace('%20','\ ').replace('\n',' // '))
 		if not reduce_spaces_all(msg).split('->',1)[1]: msg = msg.split('->',1)[0]
+		if flag and acl_cmd not in['msg','message']:
+			mb = [t[1:] for t in megabase if t[0] == jid and getRoom(t[4]) != getRoom(Settings['jid'])]
+			a = [(acl_cmd, acl_sub_act, text[0], ' '.join(text[1:]).replace('%20','\ '), 0)]				
+			was_joined = True
+			for t in mb:
+				room, realjid, nick = jid, t[3], t[0]
+				mass = cur_execute_fetchone('select message from age where jid=%s and room=%s and nick=%s',(getRoom(realjid),room,nick))[0].split('\n',4)
+				mass = (mass[4], mass[0], mass[1])
+				acl_selector(a,room,realjid,nick,mass,was_joined)
 	if silent: return L('done')
 	else: return msg
 
@@ -161,7 +170,7 @@ def bool_compare(a,b,c):
 		or (b == '>=' and a >= c)
 	
 def acl_presence(room,jid,nick,type,mass):
-	global iq_request,acl_ver_tmp,conn
+	global iq_request,acl_ver_tmp
 	#if get_level(room,nick)[0] < 0: return
 	if getRoom(jid) == getRoom(Settings['jid']): return
 	was_joined = not mass[7] or is_start
@@ -173,60 +182,63 @@ def acl_presence(room,jid,nick,type,mass):
 	# actions only on joins
 	#if was_joined: return
 	a = cur_execute_fetchall('select action,type,text,command,time from acl where jid=%s and (action like %s or action like %s or action like %s or action like %s or action like %s or action like %s or action=%s or action=%s or action=%s or action=%s or action=%s or action=%s)',(room,'prs%','presence%','nick%','all%','role%','affiliation%','jid','jidfull','res','age','ver','version'))
-	if a:
-		for tmp in a:
-			if tmp[0] == 'age':
-				past_age = cur_execute_fetchone('select sum(age) from age where room=%s and jid=%s and status=%s',(room,getRoom(jid),1))
-				now_age  = cur_execute_fetchone('select time,age from age where room=%s and jid=%s and status=%s',(room,getRoom(jid),0))
-				r_age = 0
-				if past_age or now_age:
-					try: r_age += past_age[0]
-					except: pass
-					try: r_age += int(time.time())-now_age[0]+now_age[1]
-					except: pass
+	if a: acl_selector(a,room,jid,nick,mass,was_joined)
+
+def acl_selector(a,room,jid,nick,mass,was_joined):
+	global iq_request,acl_ver_tmp
+	for tmp in a:
+		if tmp[0] == 'age':
+			past_age = cur_execute_fetchone('select sum(age) from age where room=%s and jid=%s and status=%s',(room,getRoom(jid),1))
+			now_age  = cur_execute_fetchone('select time,age from age where room=%s and jid=%s and status=%s',(room,getRoom(jid),0))
+			r_age = 0
+			if past_age or now_age:
+				try: r_age += past_age[0]
+				except: pass
+				try: r_age += int(time.time())-now_age[0]+now_age[1]
+				except: pass
+			break
+
+	for tmp in a:
+		if tmp[0] in ['ver','version'] and was_joined:
+			try: r_ver = acl_ver_tmp['%s/%s' % (room,nick)]
+			except:
+				iqid,who = get_id(), '%s/%s' % (room,nick)
+				i = Node('iq', {'id': iqid, 'type': 'get', 'to':who}, payload = [Node('query', {'xmlns': NS_VERSION},[])])
+				iq_request[iqid]=(time.time(),acl_version_async,[a, nick, jid, room, mass[0]])
+				sender(i)
+				r_ver = None
+				pprint('*** ACL version request for %s/%s' % (room,nick),'purple')
 				break
 
-		for tmp in a:
-			if tmp[0] in ['ver','version'] and was_joined:
-				try: r_ver = acl_ver_tmp['%s/%s' % (room,nick)]
-				except:
-					iqid,who = get_id(), '%s/%s' % (room,nick)
-					i = Node('iq', {'id': iqid, 'type': 'get', 'to':who}, payload = [Node('query', {'xmlns': NS_VERSION},[])])
-					iq_request[iqid]=(time.time(),acl_version_async,[a, nick, jid, room, mass[0]])
-					sender(i)
-					r_ver = None
-					pprint('*** ACL version request for %s/%s' % (room,nick),'purple')
-					break
-
-		for tmp in a:
-			itm = ''
-			if tmp[4] <= time.time() and tmp[4]:
-				cur_execute('delete from acl where jid=%s and action=%s and type=%s and text=%s',(room,tmp[0],tmp[1],tmp[2]))
-			if tmp[0].split('_')[0] in ['presence','prs'] and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[0]
-			elif tmp[0].split('_')[0] == 'nick' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = nick
-			elif tmp[0].split('_')[0] == 'role' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[1]
-			elif tmp[0].split('_')[0] == 'affiliation' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[2]
-			elif tmp[0].split('_')[0] == 'all' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = '|'.join((jid,nick,mass[0],mass[1],mass[2]))
-			elif tmp[0] == 'jid' and was_joined: itm = getRoom(jid)
-			elif tmp[0] == 'jidfull' and was_joined: itm = jid
-			elif tmp[0] == 'res' and was_joined: itm = getResourse(jid)
-			elif tmp[0] in ['ver','version'] and was_joined and r_ver: itm = r_ver
-			elif tmp[0] == 'age' and was_joined and bool_compare(r_age,tmp[1],tmp[2]):
+	for tmp in a:
+		itm = ''
+		if tmp[4] <= time.time() and tmp[4]:
+			cur_execute('delete from acl where jid=%s and action=%s and type=%s and text=%s',(room,tmp[0],tmp[1],tmp[2]))
+		if tmp[0].split('_')[0] in ['presence','prs'] and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[0]
+		elif tmp[0].split('_')[0] == 'nick' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = nick
+		elif tmp[0].split('_')[0] == 'role' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[1]
+		elif tmp[0].split('_')[0] == 'affiliation' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = mass[2]
+		elif tmp[0].split('_')[0] == 'all' and (('_join' in tmp[0] and was_joined) or ('_change' in tmp[0] and not was_joined) or ('_join' not in tmp[0] and '_change' not in tmp[0])): itm = '|'.join((jid,nick,mass[0],mass[1],mass[2]))
+		elif tmp[0] == 'jid' and was_joined: itm = getRoom(jid)
+		elif tmp[0] == 'jidfull' and was_joined: itm = jid
+		elif tmp[0] == 'res' and was_joined: itm = getResourse(jid)
+		elif tmp[0] in ['ver','version'] and was_joined and r_ver: itm = r_ver
+		elif tmp[0] == 'age' and was_joined and bool_compare(r_age,tmp[1],tmp[2]):
+			acl_action(tmp[3],nick,jid,room,None)
+			break
+		if itm:
+			if tmp[1] == 'exp' and re.match(tmp[2].replace('*','*?'),itm,re.I+re.S+re.U):
 				acl_action(tmp[3],nick,jid,room,None)
 				break
-			if itm:
-				if tmp[1] == 'exp' and re.match(tmp[2].replace('*','*?'),itm,re.I+re.S+re.U):
-					acl_action(tmp[3],nick,jid,room,None)
-					break
-				elif tmp[1] == 'cexp' and re.match(tmp[2].replace('*','*?'),itm,re.S+re.U):
-					acl_action(tmp[3],nick,jid,room,None)
-					break
-				elif tmp[1] == 'sub' and tmp[2].lower() in itm.lower():
-					acl_action(tmp[3],nick,jid,room,None)
-					break
-				elif itm.lower() == tmp[2].lower() or (tmp[0] == 'all' and tmp[2].lower() in (jid.lower(),nick.lower(),mass[0].lower())):
-					acl_action(tmp[3],nick,jid,room,None)
-					break
+			elif tmp[1] == 'cexp' and re.match(tmp[2].replace('*','*?'),itm,re.S+re.U):
+				acl_action(tmp[3],nick,jid,room,None)
+				break
+			elif tmp[1] == 'sub' and tmp[2].lower() in itm.lower():
+				acl_action(tmp[3],nick,jid,room,None)
+				break
+			elif itm.lower() == tmp[2].lower() or (tmp[0] == 'all' and tmp[2].lower() in (jid.lower(),nick.lower(),mass[0].lower())):
+				acl_action(tmp[3],nick,jid,room,None)
+				break
 
 def acl_version_async(a, nick, jid, room, mass, is_answ):
 	global acl_ver_tmp
@@ -239,7 +251,7 @@ def acl_version_async(a, nick, jid, room, mass, is_answ):
 			if tmp[1] == 'exp' and re.match(tmp[2].replace('*','*?'),itm,re.I+re.S+re.U): acl_action(tmp[3],nick,jid,room,None)
 			elif tmp[1] == 'cexp' and re.match(tmp[2].replace('*','*?'),itm,re.S+re.U): acl_action(tmp[3],nick,jid,room,None)
 			elif tmp[1] == 'sub' and tmp[2].lower() in itm.lower(): acl_action(tmp[3],nick,jid,room,None)
-			elif itm.lower() == tmp[2].lower() or (tmp[0] == 'all' and tmp[2].lower() in (jid.lower(),nick.lower(),mass.lower())): acl_action(tmp[3],nick,jid,room,None)
+			elif itm.lower() == tmp[2].lower() or (tmp[0] == 'all' and tmp[2].lower() in (jid.lower(),nick.lower(),'\n'.join(mass).lower())): acl_action(tmp[3],nick,jid,room,None)
 
 global execute, presence_control, message_act_control
 
