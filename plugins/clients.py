7733c49842c973038155f088231d1a27aaa65363
disabler <dissy@ya.ru>
2012-01-04 13:28:38 +0400

    fix: answers in clients stats

--- plugins/clients.py
+++ plugins/clients.py
@@ -21,26 +21,31 @@
 #                                                                             #
 # --------------------------------------------------------------------------- #
 
-def hidden_clear(type, jid, nick, text):
+def clients_stats(type, jid, nick, text):
 	text = reduce_spaces_all(text).lower().split()
-	if len(text) == 2: arg,match = text
-	else: arg,match = '',text[0]
-	if arg in ['all','total','global']: req,par = 'select message from age where message like %s order by -time,-status',('%\r%',)
-	else: req,par = 'select message from age where room=%s message like %s order by -time,-status',(jid,'%\r%')
+	if text:
+		if len(text) == 1: arg,match = text[0],'%\r%'
+		elif len(text) == 2: arg,match = text[0], '%%\r%%%s%%' % text[1]
+		else: arg,match = text[0], '%%\r%%%s%%' % ' '.join(text[1:])
+	else: arg,match = '','%\r%'
+	if arg.split('-')[0] in ['all','total','global']: req,par = 'select message from age where message ilike %s',(match,)
+	else: req,par = 'select message from age where room=%s message ilike %s',(jid,match)
 	st = cur_execute_fetchall(req,par)
 	if st:
 		ns = {}
+		et = L('Error! %s')%''
 		for t in st:
-			k = t.split('\r',1)[1].split(' // ')[0]
-			if not k.startswith(L('Error! %s')%''):
+			k = t[0].split('\r',1)[1].split(' // ')[0].replace('\r','[LF]').replace('\n','[CR]').replace('\t','[TAB]')
+			if '-' in arg and arg.split('-')[1] == 'short': k = k.split()[0]
+			if not k.startswith(et.strip()):
 				if ns.has_key(k): ns[k] += 1
 				else: ns[k] = 1
 		ns = [(ns[t],t) for t in ns.keys()]
-		ns.sort()
-		msg = L('Client statistic:\n%s') % '\n'.join(['%s\t%s' % (t[1],t[0]) for t in ns[:10]])
+		ns.sort(reverse=True)
+		msg = L('Client statistic:\n%s') % '\n'.join(['%s. %s\t%s' % (ns.index(t)+1,t[1],t[0]) for t in ns[:10]])
 	else: msg = L('Clients statistic not available.')
 	send_msg(type, jid, nick, msg)
 
 global execute
 
-execute = [(4, 'clients', clients_stats, 2, L('Show clients statistic when available.'))]
+execute = [(4, 'clients', clients_stats, 2, L('Show clients statistic when available.\nclients [total|global|all[-short]] [string]'))]
