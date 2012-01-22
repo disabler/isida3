771c1c689c59fc9d6ce9f19fb03028731aaab2d3
diSabler <dissy@ya.ru>
2012-01-22 13:05:40 +0400

    fix: rewrite `domain_info` command

--- plugins/domain.py
+++ plugins/domain.py
@@ -25,27 +25,14 @@ def domaininfo(type, jid, nick, text):
 	text = text.strip().lower()
 	while ' \n' in text: text = text.replace(' \n','\n')
 	if ''.join(re.findall(u'[-0-9a-zа-я. ]+',text,re.U+re.I)) == text and text.count('.') >= 1 and len(text) > 4:
-		if 'sites' in text: t, text, regex, msg = 2, text.split(' ')[1], '<font color="black" size="2">(.*?)</font>', L('Sites at Domain/IP address: %s\n') % text.split(' ')[1]
-		else: t, regex, msg = 1, '<blockquote>(.*?)</blockquote>', L('Domain/IP address info:')
+		if text.startswith('sites '): t, text, regex = 2, text.split(' ')[1], '<h3>(.*?)<br><br><br>'
+		else: t, regex = 1, '<h3>(.*?)</a><br><br /><span id="av">'
 		url = 'http://1whois.ru/index.php?url=%s&t=%s' % (text.encode('idna'),t)
 		body = deidna(html_encode(load_page(url).replace('&nbsp;', ' ')))
 		try:
 			body = re.findall(regex, body, re.S)[0]
-			if u'Нет данных!' in body: msg += ' '+ L('No data!')
-			else:
-				tmp_body = {}
-				body = replacer(body).split('\n')
-				newbody = []
-				for tmp in body:
-					if ':' in tmp:
-						if len(tmp.split(':')[0].split()) <= 2 and len(tmp.split(':')[1]) >= 2:
-							if not tmp_body.has_key(tmp.split(':')[1]):
-								tmp_body[tmp.split(':')[1]]=tmp.split(':')[0]
-								newbody.append(tmp)
-					elif tmp.count(' ') <= 4 and not tmp_body.has_key(tmp):
-							tmp_body[tmp] = tmp
-							newbody.append(tmp)
-				msg += '\n'+'\n'.join(newbody)
+			if body: msg = unhtml_hard(''.join(body))
+			else: msg = L('No data!')
 		except: msg = L('Unexpected error')
 	else: msg = L('Error!')
 	send_msg(type, jid, nick, msg)
