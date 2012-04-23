4a9b1335eb4e36699fe2b2cac245989f1593eafc
diSabler <dissy@ya.ru>
2012-04-24 00:43:09 +0400

    add: resend new hash after soft update

--- plugins/shutdown.py
+++ plugins/shutdown.py
@@ -46,6 +46,16 @@ def bot_soft_update(type, jid, nick, text):
 			send_msg(type, jid, nick, L('Soft update finished! Plugins loaded: %s. Commands: %s') % (len(plugins)+1,len(comms)))
 			break
 		else: time.sleep(1)
+	pprint('*** Send new hash in rooms')
+	for tocon in confbase:
+		tocon = tocon.strip()
+		if '\n' in tocon: pprint('->- %s | pass: %s' % tuple(tocon.split('\n',1)),'green')
+		else: pprint('->- %s' % tocon,'green')
+		baseArg = unicode(tocon)
+		if '/' not in baseArg: baseArg += '/%s' % unicode(Settings['nickname'])
+		if '\n' in baseArg: baseArg,passwd = baseArg.split('\n',2)
+		else: passwd = ''
+		zz = join(baseArg, passwd)
 	if Settings['status'] == 'online': caps_and_send(xmpp.Presence(status=Settings['message'], priority=Settings['priority']))
 	else: caps_and_send(xmpp.Presence(show=Settings['status'], status=Settings['message'], priority=Settings['priority']))
 	
