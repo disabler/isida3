d2d1453db4d0e55b130aaae88f76ba98f00a242f
diSabler <dissy@ya.ru>
2012-05-05 01:49:22 +0400

    fix: year format in time show func

--- kernel.py
+++ kernel.py
@@ -589,7 +589,7 @@ def paste_text(text,room,jid):
 
 def disp_time(t):
 	lt=tuple(time.localtime(t))
-	return '%02d:%02d:%02d, %02d.%s\'%02d, %s' % (lt[3],lt[4],lt[5],lt[2],wmonth[lt[1]-1],lt[0],wday[lt[6]])
+	return '%02d:%02d:%02d, %02d.%s\'%s, %s' % (lt[3],lt[4],lt[5],lt[2],wmonth[lt[1]-1],lt[0],wday[lt[6]])
 
 def nice_time(ttim):
 	gt=tuple(time.gmtime())
@@ -597,8 +597,8 @@ def nice_time(ttim):
 	timeofset = int(round(int(time.mktime(lt[:5]+(0,0,0,0))-time.mktime(gt[:5]+(0,0,0,0)))/3600.0))
 	if timeofset < 0: t_gmt = 'GMT%s' % timeofset
 	else: t_gmt = 'GMT+%s' % timeofset
-	t_utc='%02d%02d%02dT%02d:%02d:%02d' % (gt[0],gt[1],gt[2],gt[3],gt[4],gt[5])
-	t_display = '%02d:%02d:%02d, %02d.%s\'%02d, %s, ' % (lt[3],lt[4],lt[5],lt[2],wmonth[lt[1]-1],lt[0],wday[lt[6]])
+	t_utc='%s%02d%02dT%02d:%02d:%02d' % gt[:6]
+	t_display = '%02d:%02d:%02d, %02d.%s\'%s, %s, ' % (lt[3],lt[4],lt[5],lt[2],wmonth[lt[1]-1],lt[0],wday[lt[6]])
 	#t_tz = time.tzname[time.localtime()[8]]
 	#enc = chardet.detect(t_tz)['encoding']
 	#if t_tz == None: body = ''
