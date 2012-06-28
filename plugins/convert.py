ca1321822c825858998e8c51c3e2ed06654c664a
diSabler <dissy@ya.ru>
2012-06-28 22:42:34 +0400

    fix: convert command

--- plugins/convert.py
+++ plugins/convert.py
@@ -24,14 +24,14 @@
 currency_conv_list = ['ATS','AUD','BEF','BYR','CAD','CHF','CNY','DEM','DKK','EEK',\
 					  'EGP','ESP','EUR','FIM','FRF','GBP','GRD','IEP','ISK','ITL',\
 					  'JPY','KGS','KWD','KZT','LTL','NLG','NOK','PTE','SDR','SEK',\
-					  'SGD','TRL','TRY','UAH','USD','XDR','YUN','BASE']
+					  'SGD','TRL','TRY','UAH','USD','XDR','YUN','RUR']
 
 def currency_converter(type, jid, nick, text):
 	msg = L('Error in parameters. Read the help about command.')
 	text = text.strip().upper()
-	if text == 'LIST': msg = L('Available values:\n%s') % ', '.join(currency_conv_list).replace('BASE','RUR')
+	if text == 'LIST': msg = L('Available values:\n%s') % ', '.join(currency_conv_list)
 	else:
-		repl_curr = ((u'€','EUR'),('$','USD'),(u'¥','JPY'),(u'£','GBP'),('RUR','BASE'),(',','.'))
+		repl_curr = ((u'€','EUR'),('$','USD'),(u'¥','JPY'),(u'£','GBP'),(',','.'))
 		for tmp in repl_curr: text = text.replace(tmp[0],' %s ' % tmp[1])
 		c_from,c_to,c_summ = '','',''
 		val = [t for t in re.findall('[A-Z]{3,4}', text, re.S) if t in currency_conv_list]
@@ -43,15 +43,29 @@ def currency_converter(type, jid, nick, text):
 			else: c_summ = val
 		if c_from and c_to and c_summ:
 			date = tuple(time.localtime())[:3]
-			url = 'http://cash.rbc.ru/converter.shtml?mode=calc&source=cb.0&tid_from=%s&commission=1&tid_to=%s&summa=%s&day=%s&month=%s&year=%s' % (c_from,c_to,c_summ,date[2],date[1],date[0])
+			mname = ['%D0%AF%D0%BD%D0%B2%D0%B0%D1%80%D1%8C','%D0%A4%D0%B5%D0%B2%D1%80%D0%B0%D0%BB%D1%8C',
+					 '%D0%9C%D0%B0%D1%80%D1%82','%D0%90%D0%BF%D1%80%D0%B5%D0%BB%D1%8C',
+					 '%D0%9C%D0%B0%D0%B9','%D0%98%D1%8E%D0%BD%D1%8C',
+					 '%D0%98%D1%8E%D0%BB%D1%8C','%D0%90%D0%B2%D0%B3%D1%83%D1%81%D1%82',
+					 '%D0%A1%D0%B5%D0%BD%D1%82%D1%8F%D0%B1%D1%80%D1%8C','%D0%9E%D0%BA%D1%82%D1%8F%D0%B1%D1%80%D1%8C',
+					 '%D0%9D%D0%BE%D1%8F%D0%B1%D1%80%D1%8C','%D0%94%D0%B5%D0%BA%D0%B0%D0%B1%D1%80%D1%8C']
+			url = 'http://quote.rbc.ru/cgi-bin/conv/external/converter.shtml?source=cb.0&tid_from=%s&commission=1.0&tid_to=%s&summa=%s&day=%s&mon=%s&year=%s' % (c_from,c_to,c_summ,date[2],mname[date[1]-1],date[0])
 			body = html_encode(load_page(url))
+			try:
+				curr = body.split('<select name="tid_from" class="n">')[1].split('</select>')[0]
+				curr_list = dict(re.findall('<option value="(.*?)".*?>(.*?)</option>',curr,re.S+re.I+re.U))
+			except: curr_list = {} 
 			try: body = body.split('<table id="rTable" class="table">',1)[1]
 			except: body = ''
 			regex = '<tbody>.*?<td>(.*?)</td>.*?<td class="b">(.*?)</td>.*?<td class="b">(.*?)</td>.*?<td>(.*?)</td>.*?<td class="b">(.*?)</td>'
 			mt = re.findall(regex, body, re.S)
 			if mt != []:
 				mt = [t.strip() for t in mt[0]]
-				msg = '%s %s (%s) = %s %s (%s) | 1 %s = %s %s' % (c_summ,c_from,mt[0],mt[4],c_to,mt[3],c_from,mt[2],c_to)
+				if curr_list.has_key(mt[0]): frv = curr_list[mt[0]]
+				else: frv = mt[0]
+				if curr_list.has_key(mt[3]): tov = curr_list[mt[3]]
+				else: tov = mt[3]
+				msg = '%s %s (%s) = %s %s (%s) | 1 %s = %s %s' % (c_summ,c_from,frv,mt[4],c_to,tov,c_from,mt[2],c_to)
 				msg = msg.replace('BASE','RUR')
 	send_msg(type, jid, nick, msg)
 
