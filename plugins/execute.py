a4ae9d283fcb4209ace948a44f7d0e6c99354adf
diSabler <dissy@ya.ru>
2013-03-24 02:01:27 +0400

    add: dpi calculator

--- plugins/execute.py
+++ plugins/execute.py
@@ -60,6 +60,24 @@ def calc(type, jid, nick, text):
 		else: calc_last_res[jid] = {nick: None}
 	send_msg(type, jid, nick, text)
 
+def dpi_calc(type, jid, nick, text):
+	text = text.strip().replace(',','.')
+	if text:
+		tupl = re.findall('([0-9.]+)',text)[:3]
+		if len(tupl) == 3:
+			if '.' in tupl[0] or '.' in tupl[1]: msg = L('Width and height must be integer!','%s/%s'%(jid,nick))
+			elif not float(tupl[2]): msg = L('Incorrect diagonal value!','%s/%s'%(jid,nick))
+			else:
+				dpi_type = [0,'L'],[160,'M'],[240,'H'],[320,'XH']
+				dpi = int((math.sqrt(int(tupl[0])**2+int(tupl[1])**2))/float(tupl[2]))
+				dpi_name = 'unknown'
+				for t in dpi_type:
+					if dpi > t[0]: dpi_name = '%sDPI' % t[1]
+				msg = '%s %sx%sx%s\" - %sdpi [%s]' % (L('Screen','%s/%s'%(jid,nick)),tupl[0],tupl[1],tupl[2],dpi,dpi_name)
+		else: msg = L('Not enough parameters!','%s/%s'%(jid,nick))
+	else: msg = L('What?','%s/%s'%(jid,nick))
+	send_msg(type, jid, nick, msg)
+
 def calc_clear(room,jid,nick,type,arr):
 	if type == 'unavailable' and calc_last_res.has_key(room) and calc_last_res[room].has_key(nick): del calc_last_res[room][nick]
 
@@ -69,6 +87,6 @@ presence_control = [calc_clear]
 
 if not GT('paranoia_mode'): execute = [(3, 'calc', calc, 2, L('Calculator.')),
 	(9, 'exec', exec_ute, 2, L('Execution of external code.'))]
+else: execute = []
 
-
-
+execute.append((3, 'dpi', dpi_calc, 2, L('DPI calculator.\ndpi <width height size>')))
