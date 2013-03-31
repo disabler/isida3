99487baa02cff6f40194d0476dbf9b167c083943
diSabler <dissy@ya.ru>
2013-03-31 11:11:28 +0400

    fix: regexp in train command

--- plugins/train.py
+++ plugins/train.py
@@ -26,12 +26,12 @@ def train(type, jid, nick, text):
 		from_to = map(lambda x: urllib.quote(x.encode('utf-8')), text.strip().split(' - '))
 		url = 'http://rasp.yandex.ru/search/train/?fromName=%s&toName=%s' % (from_to[0], from_to[1])
 		data = html_encode(load_page(url))
-		res = re.findall('<a class="b-link" href="/thread/.+?><strong>(.+?)</strong> <span class="g-nowrap">(.+?)</span>.+?<span class="g-nowrap">(.+?)</span></a>.+?<strong>(.+?)</strong>.+?<strong>(.+?)</strong>.+?<div class="b-timetable__pathtime">\s+(.+?)<span class="b-timetable__mark">(.+?)</span>', data, re.I+re.S+re.U)
+		data = data.split('<div class="b-timetable__tripname">',3)[1]
+		res = re.findall('<a class="b-link" href="/thread/.+?><strong>(.+?)</strong> <span class="g-nowrap">(.+?)</span>.+?<span class="g-nowrap">(.+?)</span></a>.+?<strong>(.+?)</strong>.+?<strong>(.+?)</strong>.+?<div class="b-timetable__pathtime">(.+?)</div>', data, re.I+re.S+re.U)
 		if res.count(res[0]) > 1: res = res[:res.index(res[0], 1)]
-		msg = '\n%s' % '\n'.join([u'%s\t%s-%s\t\t%s-%s\t(%s%s в пути)' % _ for _ in res])
+		msg = '\n'.join([u'%s\t%s-%s\t\t%s-%s\t(%s в пути)' % _ for _ in res])
 		if not msg.strip() or 'b-pseudo-link js-transfers-trigger' in data: msg = L('What?','%s/%s'%(jid,nick))
-	except:
-		msg = L('Command execution error.','%s/%s'%(jid,nick))
+	except: msg = L('Command execution error.','%s/%s'%(jid,nick))
 	send_msg(type,jid,nick,msg)
 
 global execute
