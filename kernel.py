849601ce43235128a3bc121eb407eef3ffbb3810
diSabler <dissy@ya.ru>
2013-09-22 10:52:41 +0400

    add: stack size in threading

--- kernel.py
+++ kernel.py
@@ -1715,6 +1715,9 @@ if base_type in ['mysql','sqlite3']:
 if thread_type:
 	import threading
 	sema = threading.BoundedSemaphore(value=30)
+	try: threading.stack_size(32768)
+	except ValueError: pass
+
 	class KThread(threading.Thread):
 		def __init__(self, *args, **keywords):
 			threading.Thread.__init__(self, *args, **keywords)
