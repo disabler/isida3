93ac8c34b08177dfadc44a6b93c2fdacd4192d43
VitaliyS <vitaliy@root.ua>
2014-02-06 00:10:43 +0200

    upd: kinopoisk.py

--- plugins/kinopoisk.py
+++ plugins/kinopoisk.py
@@ -22,31 +22,31 @@
 # --------------------------------------------------------------------------- #
 
 kinopoisk_error_string = '<META HTTP-EQUIV="Pragma" CONTENT="no-cache">'
-kinopoisk_error_regexp = '<td style="padding-left:10px">(.*?)<br><br><small>'
 
 def kinopoisk(type, jid, nick, text):
-	text=text.strip().split(' ', 1)
-	if len(text) == 2 and not text[0] in ['id', 'search'] or len(text) == 1: text = [' ', ' '.join(text)]
-	if text[0] == 'search' or text[0] == ' ' and not re.search('^\d+$', text[-1]):
-		query=urllib.quote(text[-1].encode('cp1251'))
+	text=text.strip()
+	if len(text) > 2:
+		query=urllib.quote(text.encode('cp1251'))
 		data = html_encode(load_page('http://m.kinopoisk.ru/search/'+query))
 		if kinopoisk_error_string in data: msg = unhtml_hard(re.findall(kinopoisk_error_regexp,data,re.I+re.U+re.S)[0])
 		else:
 			temp_urls = re.findall('<a href="http://m.kinopoisk.ru/movie/(\d+?)/">(.+?)</a>', data)
 			if temp_urls:
 				msg = L('Found:','%s/%s'%(jid,nick))
-				for t_u in temp_urls:
-					msg += '\n%s - %s' % (t_u[0], t_u[1])
+				for n, t_u in enumerate(temp_urls):
+					rs = ''
+					if n < 3:
+						try:
+							r = load_page('http://www.kinopoisk.ru/rating/%s.xml' % t_u[0])
+							r = re.findall('>([\.\d]+?)<', r)
+							rs = ' (KP: %s / IMDB: %s)' % (r[0], r[1])
+						except:
+							pass
+					msg += '\nhttp://www.kinopoisk.ru/film/%s/ - %s%s' % (t_u[0], t_u[1], rs)
 			else: msg = L('Not found!','%s/%s'%(jid,nick))
-	elif re.search('^\d+$', text[-1]):
-		data = html_encode(load_page('http://m.kinopoisk.ru/movie/'+text[-1]))
-		if kinopoisk_error_string in data: msg = unhtml_hard(re.findall(kinopoisk_error_regexp,data,re.I+re.U+re.S)[0])
-		else:
-			tmp = unhtml_hard(re.search('<p class="title">((?:.|\s)+?)</div>', data).group(1)).split('\n')
-			msg = '\n'.join([i[0].upper()+i[1:] for i in tmp])
 	else: msg = L('What?','%s/%s'%(jid,nick))
 	send_msg(type,jid,nick,msg)
 
 global execute
 
-execute = [(0, 'film', kinopoisk, 2, 'Search in www.kinopoisk.ru. Example:\nfilm [id] film_id\nfilm [search] film_name')]
+execute = [(0, 'film', kinopoisk, 2, 'Search in www.kinopoisk.ru. Example:\nfilm film_name')]
